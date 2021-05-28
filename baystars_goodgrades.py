af76f2500dda206ac4514d524177f1d669ef616d
ryotaro-0343 <author_0gg11v4y726162p@yahoo.ne.jp>
2021-05-28 22:45:25 +0900

    ボタンを押したときの処理を3つの関数に分けた

--- baystars_goodgrades.py
+++ baystars_goodgrades.py
@@ -7,9 +7,6 @@ from bs4 import BeautifulSoup
 ベイスターズの打者の良い成績だけを公式サイトから持ってくるコード。
 サイトを見たところ,成績の情報が<td>というタグにまとめられていることが分かった。
 一人の打者につき5つずつのタグがあったのでそれぞれの情報を(5x+n)で表すようにした。
-また、ボタンを押された時の処理を見やすくしようと思い、成績の情報を集める関数と、
-ボタンを押された時の処理の関数を分けようとしたが、変数の情報を保持できなかったので、
-ボタンを押された時の処理で一度に両方行った。
 """
 
 #　メインウィンドウを作る
@@ -17,15 +14,18 @@ win = Tk()
 mylabel = Label(win,text = "ベイスターズの試合情報を見ますか？")
 mylabel.pack()
 
-def ok_click():
+def get_soup():
+    # URLから情報を取ってきて、テキストとして返す
+    url = 'https://www.baystars.co.jp/game/result'
+    r = requests.get(url)
+    return BeautifulSoup(r.text)
+
+
+def db_grade():
+    #　打者のリスト
     db_butterlist = ['戸柱','伊藤光','益子','高城','嶺井','山本','東妻','中井','牧','伊藤裕',
     '倉本','森','大和','柴田','田中俊','小深田','宮崎','田部','知野','山下','ソト','デラロサ',
     '宮本','桑原','佐野','神里','オースティン','乙坂','楠本','細川','蛯名','関根']
-
-    # URLからTHMLの情報をテキストとして持ってくる
-    url = 'https://www.baystars.co.jp/game/result'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text)
 
     count = 0           # 成績を判別する際に必要な数字
     is_db = False       # ベイスターズの選手かの判別
@@ -39,7 +39,7 @@ def ok_click():
         pass
 
     # tdタグに挟まれている箇所を抽出
-    found = soup.find_all('td')
+    found = get_soup().find_all('td')
     # 抽出した箇所を一つずつ処理する
     for child in found:
         count += 1
@@ -87,17 +87,21 @@ def ok_click():
 
     # 処理の最初にカウントを+1しているので、カウントが1以上なら試合が始まっていると分かる
     if count >= 1:
-        game = True
+        return True
+    elif count == 0:
+        return False
 
+def ok_click():
     # 試合が始まっているとき、ファイルから成績を取ってきて、試合情報というタイトルで成績を表示
-    if game == True:
+    if db_grade() == True:
         with open('test.text') as f:
             s = f.read()
             mb.showinfo('試合情報',s)
+
     # 試合が始まっていないときの処理
-    else:
+    elif db_grade() == False:
         #　試合開始時刻がinfomation--dateクラスの1番目の検索結果の先頭にあるので表示する
-        start = soup.select('.information--date')
+        start = get_soup().select('.information--date')
         start_time = start[0]
         mb.showinfo('試合情報','試合開始時刻は {0} です'.format(start_time.text[:6]))
 
